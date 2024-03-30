2fb901383be20b5d896ec64fb8e85079d0ca73d8
Simone-shalom <studzinskis46@gmail.com>
2024-03-30 17:00:38 +0100

    feat:Created reusable Button comp

--- strapi-backend/types/generated/components.d.ts
+++ strapi-backend/types/generated/components.d.ts
@@ -1,5 +1,18 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface ElementsButtonLink extends Schema.Component {
+  collectionName: 'components_elements_button_links';
+  info: {
+    displayName: 'Button Link';
+  };
+  attributes: {
+    title: Attribute.String;
+    link: Attribute.String;
+    isExternal: Attribute.Boolean & Attribute.DefaultTo<false>;
+    type: Attribute.Enumeration<['primary', 'secondary']>;
+  };
+}
+
 export interface SeoMetadata extends Schema.Component {
   collectionName: 'components_seo_metadata';
   info: {
@@ -15,6 +28,7 @@ export interface SeoMetadata extends Schema.Component {
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
+      'elements.button-link': ElementsButtonLink;
       'seo.metadata': SeoMetadata;
     }
   }
